1edea642f6f0d87c24d7065e69dcda3e2e3e70f2
Amir Espahbodi <dev.amir.espahbodi@gmail.com>
2024-11-17 04:05:12 +0100

    add necessary packages

---
+++ run.py
@@ -0,0 +1,9 @@
+import asyncio
+
+async def main():
+    pass
+
+if __name__ == "__main__":
+    city = input("enter city name: ")
+    title = input("enter place base title: ")
+    asyncio.run(main())
